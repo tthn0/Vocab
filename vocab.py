7a152244a667007f20eb4153f88106934a61e782
Saif Ul Islam <saifulislam84210@gmail.com>
2020-06-07 15:11:16 +0500

    Surrounded in try, except clause
    Helps to fail silently, else throws a verbose output when not connected to the internet.

--- vocab.py
+++ vocab.py
@@ -20,17 +20,21 @@ darkgrey = '\033[90m'
 # print(f"{cyan}{word}{reset}: {darkgrey}{italic}{bold}{kind}{reset} {definition[2:].strip()}")
 
 URL = 'https://www.dictionary.com/e/word-of-the-day/'
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, 'html.parser')
+page = None
+try:
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-word = soup.find('div', class_='wotd-item-headword__word').text.strip()
-pron = soup.find('div', class_='wotd-item-headword__pronunciation').text.strip()
-kind = soup.find('span', class_='luna-pos').text.strip()
-definition = soup.find('div', class_='wotd-item-headword__pos').find_all('p')[1].text.strip()
+    word = soup.find('div', class_='wotd-item-headword__word').text.strip()
+    pron = soup.find('div', class_='wotd-item-headword__pronunciation').text.strip()
+    kind = soup.find('span', class_='luna-pos').text.strip()
+    definition = soup.find('div', class_='wotd-item-headword__pos').find_all('p')[1].text.strip()
 
-pron = pron[0:1:] + pron[2::]
-pron = pron[0:-2:] + pron[-1::]
+    pron = pron[0:1:] + pron[2::]
+    pron = pron[0:-2:] + pron[-1::]
 
-print(bold + cyan + word + reset + ' ' + pron + ': ' + italic + darkgrey + kind + reset)
-print(definition)
-print()
+    print(bold + cyan + word + reset + ' ' + pron + ': ' + italic + darkgrey + kind + reset)
+    print(definition)
+    print()
+except:
+    pass
